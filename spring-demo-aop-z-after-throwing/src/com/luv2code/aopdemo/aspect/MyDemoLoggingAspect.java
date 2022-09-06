4b17b71efe3e10c5b4856a8b8d59851e491d76a1
kevamrein <kevamrein@gmail.com>
2022-09-06 07:03:06 -0400

    349-354

---
+++ spring-demo-aop-z-after-throwing/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,64 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	@AfterThrowing(pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDao.findAccounts(..))", throwing = "exc")
+	public void afterThrowingFindAccount(JoinPoint joinPoint, Throwable exc) {
+		MethodSignature methodSig = (MethodSignature) joinPoint.getSignature();
+		System.out.println("executing after throwing on method" + methodSig);
+		
+		System.out.println("Advice exception " + exc);
+	}
+	
+	@AfterReturning(pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDao.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+		MethodSignature methodSig = (MethodSignature) joinPoint.getSignature();
+		System.out.println("executing after return on method" + methodSig);
+		System.out.println("Result: " + result);
+		
+		convertAccountNamesToUppercase(result);
+	}
+	
+	private void convertAccountNamesToUppercase(List<Account> result) {
+		for (Account account : result) {
+			account.setName(account.getName().toUpperCase());
+		}
+	}
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint joinPoint) {
+		System.out.println("Executing Before for add account");
+		
+		MethodSignature methodSig = (MethodSignature) joinPoint.getSignature();
+	
+		Object[] args = joinPoint.getArgs();
+		
+		for (Object arg : args) {
+			System.out.println(arg);
+			
+			if (arg instanceof Account) {
+				Account account = (Account) arg;
+				System.out.println(account.getName());
+				System.out.println(account.getLevel());
+			}
+		}
+		System.out.println(methodSig.toString());
+	}
+}
